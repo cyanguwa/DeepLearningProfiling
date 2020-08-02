87b68582d235ac5b3958e8a14d0847a2b883f13e
Yunsong Wang <yunsongwang@lbl.gov>
2020-08-01 23:53:16 -0700

    Update LSTM job script

--- Basic-Kernels/scripts/rnn1d-tf1-noxla.sh
+++ Basic-Kernels/scripts/rnn1d-tf1-noxla.sh
@@ -42,6 +42,11 @@ if [ $enable_xla == "xla" ];then
     sed -i 's/allow_soft_placement=False/allow_soft_placement=True/g' $script
 fi
 
+#net_params
+net_params="16,16,32,16 32,16,32,16, 64,16,32,16 128,16,32,16 "
+net_params+="16,32,32,16 16,64,32,16 16,128,32,16 "
+net_params+="16,16,64,16 16,16,128,16 "
+net_params+="16,16,32,32 16,16,32,64 16,16,32,128 "
 
 #list of metrics
 #metrics="sm__cycles_elapsed.avg "
@@ -63,58 +68,50 @@ l1tex__t_bytes.sum "
 
 cells="lstm"
 precs="16 32"
-batch_size="32 64 128 256"
-time_steps="32 64 128"
-features="32 64 128"
-hidden_size="16 32 64 128"
-
-#cells="rnn lstm gru "
-#precs="16 "
 
 num_warmup=5
 num_iter=1
 
 for prec in $precs; do
     for cell in $cells; do
-        for batch in $batch_size; do
-            for time in $time_steps; do
-                for feature in $features; do
-                    for h_size in $hidden_size; do
-                        input_tensor_shape=$batch" "$time" "$feature
-                        echo $input_tensor_shape
+        for net_param in ${net_params}; do
+            tmp_param=(${net_param//,/ })
+            batch=${tmp_param[0]}
+            time=${tmp_param[1]}
+            feature=${tmp_param[2]}
+            h_size=${tmp_param[3]}
+            input_tensor_shape=$batch" "$time" "$feature
+            echo $input_tensor_shape
 
-                        outputstr=tf1.fp_${prec}.celltype_${cell}.input_${batch}x${time}x${feature}.nneu_${h_size}
-                        #iterate over FW BW
-                        for ctype in calibrate forward backward; do
+            outputstr=tf1.fp_${prec}.celltype_${cell}.input_${batch}x${time}x${feature}.nneu_${h_size}
+            #iterate over FW BW
+            for ctype in calibrate forward backward; do
 
-                            #profile string
-                            profilestring="/usr/common/software/cuda/11.0.167/bin/nv-nsight-cu-cli \
-                            --profile-from-start off --metrics ${metrics} --kernel-base demangled -o \
-                            ${outputstr}.pass_${ctype}.${enable_xla}"
+                #profile string
+                profilestring="/usr/common/software/cuda/11.0.167/bin/nv-nsight-cu-cli \
+                --profile-from-start off --metrics ${metrics} --kernel-base demangled -o \
+                ${outputstr}.pass_${ctype}.${enable_xla}"
 
-                            if [ $enable_xla == "xla" ];then
-                                ${sruncmd} ${profilestring} $(which python) -u ./$script \
-                                    --input_tensor_shape ${input_tensor_shape} \
-                                    --cell_type ${cell} \
-                                    --n_neurons ${h_size} \
-                                    --dtype float${prec} \
-                                    --num_iterations ${num_iter} \
-                                    --num_warmups ${num_warmup} \
-                                    --enable_xla \
-                                    --compute_type ${ctype}
-                            else
-                                ${sruncmd} ${profilestring} $(which python) -u ./$script \
-                                    --input_tensor_shape ${input_tensor_shape} \
-                                    --cell_type ${cell} \
-                                    --n_neurons ${h_size} \
-                                    --dtype float${prec} \
-                                    --num_iterations ${num_iter} \
-                                    --num_warmups ${num_warmup} \
-                                    --compute_type ${ctype}
-                            fi
-                        done
-                    done
-                done
+                if [ $enable_xla == "xla" ];then
+                    ${sruncmd} ${profilestring} $(which python) -u ./$script \
+                        --input_tensor_shape ${input_tensor_shape} \
+                        --cell_type ${cell} \
+                        --n_neurons ${h_size} \
+                        --dtype float${prec} \
+                        --num_iterations ${num_iter} \
+                        --num_warmups ${num_warmup} \
+                        --enable_xla \
+                        --compute_type ${ctype}
+                else
+                    ${sruncmd} ${profilestring} $(which python) -u ./$script \
+                        --input_tensor_shape ${input_tensor_shape} \
+                        --cell_type ${cell} \
+                        --n_neurons ${h_size} \
+                        --dtype float${prec} \
+                        --num_iterations ${num_iter} \
+                        --num_warmups ${num_warmup} \
+                        --compute_type ${ctype}
+                fi
             done
         done
     done
